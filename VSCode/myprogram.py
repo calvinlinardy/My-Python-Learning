36fefe812bfd7fe90000fc21de85afa85b11cf5e
calvinlinardy <calvin.linardy@yahoo.com>
2021-08-26 00:41:11 +0800

    Milestone 2 on the way

--- VSCode/myprogram.py
+++ VSCode/myprogram.py
@@ -1,6 +1,3 @@
-from MyMainPackage import some_main_script
-from MyMainPackage.SubPackage import mysubscript
+from mymodule import my_func
 
-some_main_script.report_main()
-
-mysubscript.sub_report()
+my_func()
