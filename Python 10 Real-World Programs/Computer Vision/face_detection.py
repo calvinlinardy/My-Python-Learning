77961f9cb9d88ea5f0d0e5db1e49a2f544feb365
Calvin Chen <calvin.linardy@yahoo.com>
2021-12-27 00:05:06 +0800

    face detection update

--- Python 10 Real-World Programs/Computer Vision/face_detection.py
+++ Python 10 Real-World Programs/Computer Vision/face_detection.py
@@ -1,3 +1,21 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier("haara")
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+img = cv2.imread(
+    "/Users/clcx/Documents/GitHub/My-Python-Learning/Python 10 Real-World Programs/Computer Vision/Pic/Divine.jpg")
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+print(type(gray_img))
+print(gray_img)
+
+faces = face_cascade.detectMultiScale(
+    gray_img, scaleFactor=1.05, minNeighbors=10)
+
+for x, y, w, h in faces:
+    img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+cv2.imshow("FACE DETECTION", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
